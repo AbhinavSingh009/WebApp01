29d5b458b3918e595fa3446c4c12c24f6fad22d5
AbhinavSingh009 <abhinav.singh1009@gmail.com>
2020-12-29 15:36:48 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,6 @@
+//const express = require('express'); common js way of doing it
+import express from 'express'; // ES6 way of doing it.
+
+const app = express();
+
+app.use(express.static('public'));
